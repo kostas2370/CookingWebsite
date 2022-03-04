71d65ce4c68eee2bcbabe915f2dbe9f8424c750e
kostas2370 <96636678+kostas2370@users.noreply.github.com>
2022-03-04 21:03:43 +0200

    Some random Changes

--- pro1/views.py
+++ pro1/views.py
@@ -3,22 +3,17 @@ from django.shortcuts import render
 
 url="https://api.chucknorris.io/jokes/random"
 
-
-
-cringe_memes=[]
-for i in range(5):
-    response = requests.get(url)
-    x= response.json()
-    cringe_memes.append(x)
-
-
-
-
 def ask1(request):
+	cringe_memes=[]
+	for i in range(5):
+		response = requests.get(url)
+		x= response.json()
+		cringe_memes.append(x)
+	
 	context={
 		'anekdota':cringe_memes
-
 	}
+	
 	return render(request,"pro1/ask1.html",context)
 
 def ask2(request):
