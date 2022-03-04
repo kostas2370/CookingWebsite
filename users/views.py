2fe7fe760ceefd3a158477095718abad0f8a0b26
kostas2370 <96636678+kostas2370@users.noreply.github.com>
2022-03-05 01:25:01 +0200

    last

--- users/views.py
+++ users/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.forms import UserCreationForm
 def register(request):
 
 	form=UserCreationForm()
-	return render(request, "users/register.html")
+	return render(request, "users/register.html" , {'form':form})
