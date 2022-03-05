c451b1501460465f929bcbba7b964a7bdf1108fd
kostas2370 <96636678+kostas2370@users.noreply.github.com>
2022-03-05 23:23:48 +0200

    Login/register

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
 			form.save()
 			username=form.cleaned_data.get("username")
 			messages.success(request,f'Acount was made for {username}')
-			return redirect("home")
+			return redirect("login")
 	else:
 		form=UserRegisterForm()
 	
